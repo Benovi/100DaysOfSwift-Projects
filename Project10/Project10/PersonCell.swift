702ffa948c5aae37010b8e5b61ea761e86654bf7
Ben Oveson <benovi@gmail.com>
2019-05-23 13:58:46 -0700

    Completed project 10.

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Ben Oveson on 5/23/19.
+//  Copyright © 2019 Ben Oveson. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var nameLbl: UILabel!
+    
+}
